c89820a142059e1356a9c8771812c23dab705c4e
KW <45307694+Asienwald@users.noreply.github.com>
2020-10-11 23:33:13 +0800

    Updates

---
+++ precache-manifest.a1ca06a3f4014b059c4f4381c3134ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b3b01fba161a7bd5923514358178e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a5ada21cfaf9520d5e",
+    "url": "/static/css/main.457e3e89.chunk.css"
+  },
+  {
+    "revision": "2cb16a8fe31ec48e538f",
+    "url": "/static/js/2.932c0cc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.932c0cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a5ada21cfaf9520d5e",
+    "url": "/static/js/main.03e83e61.chunk.js"
+  },
+  {
+    "revision": "83d861f9b783c604af74",
+    "url": "/static/js/runtime-main.068ff63e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
